b0ea8ad11bd5805401d9f88e29ffd691c19187c9
ranaabubakar5344-create <ranaabubakar5344@gmail.com>
2026-08-29 16:02:05 +0400

    ftc

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
-// import { redirect } from "next/navigation";
-
-// export default function HomePage() {
-//   redirect("/manager/login");
-// }
 import {redirect} from "next/navigation";
+
+
 
 export default function HomePage(){
   redirect("/manager/login");
--- scripts/test-db.ts
+++ scripts/test-db.ts
@@ -1,18 +1,44 @@
+// import "dotenv/config";
+// import { prisma } from "../lib/prisma";
+
+// async function main() {
+//   try {
+//     const managers = await prisma.manager.findMany();
+
+//     console.log("DB CONNECTION OK");
+//     console.log(managers);
+//   } catch (error) {
+//     console.error("DB CONNECTION FAILED");
+//     console.error(error);
+//   } finally {
+//     await prisma.$disconnect();
+//   }
+// }
+
+// main();
+
 import "dotenv/config";
-import { prisma } from "../lib/prisma";
+import {prisma} from "../lib/prisma";
 
-async function main() {
+async  function main() {
+
   try {
-    const managers = await prisma.manager.findMany();
 
-    console.log("DB CONNECTION OK");
+    const managers= await prisma.manager.findMany();
+
+    console.log("DB connection ok ");
     console.log(managers);
-  } catch (error) {
-    console.error("DB CONNECTION FAILED");
-    console.error(error);
+
+
+
+  } catch (error){
+
+  console.error("connection failed");
+  console.error(error);
+
   } finally {
     await prisma.$disconnect();
+    
   }
-}
 
-main();
+}
